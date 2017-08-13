9c8b30194a9f3c53a47e3f1b104e62c9a003c842
Julien LEREFFAIT <jlereffa@student.42.fr>
2017-08-13 13:38:08 +0200

    maj

--- srcs/evaluate_and_stock_solution.c
+++ srcs/evaluate_and_stock_solution.c
@@ -6,7 +6,7 @@
 /*   By: Nerhak <Nerhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/11 16:38:14 by Nerhak            #+#    #+#             */
-/*   Updated: 2017/08/13 10:52:28 by jlereffa         ###   ########.fr       */
+/*   Updated: 2017/08/13 13:38:05 by jlereffa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,21 @@
 int	evaluate_and_stock_solution(t_filler_var *v, int i, int j)
 {
 	if (!(v->solution = set_t_filler_solution(v->solution, v, i, j)))
+	{
+		DEB
 		return (0);
+	}
 	if (!(rewind_t_filler_solution(&v->solution)))
+	{
+		DEB
 		return (0);
+	}
 	if (!(get_solutions_values(v->solution, v)))
+	{
+		DEB
 		return (0);
-	if (find_greater_value_and_print_it(v->solution))
+	}
+	if (!find_greater_value_and_print_it(v->solution))
 		return (0);
 	return (1);
 }
