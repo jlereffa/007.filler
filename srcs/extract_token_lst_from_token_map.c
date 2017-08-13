80e8e011d9261ca2d9fc23bdb16c3ab9a9e881c5
Julien LEREFFAIT <jlereffa@student.42.fr>
2017-08-13 11:44:02 +0200

    maj

--- srcs/extract_token_lst_from_token_map.c
+++ srcs/extract_token_lst_from_token_map.c
@@ -6,7 +6,7 @@
 /*   By: jlereffa <jlereffa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/11 09:40:07 by jlereffa          #+#    #+#             */
-/*   Updated: 2017/08/13 10:27:14 by jlereffa         ###   ########.fr       */
+/*   Updated: 2017/08/13 11:38:03 by jlereffa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,12 @@ int			extract_token_lst_from_token_map(t_filler_var *v)
 		j = -1;
 		while (v->token_map[i][++j])
 		{
-			if (v->token_map[i][j] == '*')
-			{
-				if (is_origin_point && !(is_origin_point = 0))
-					set_origin_coordinates(&x_origin, &y_origin, i, j);
-				if (!(v->token = set_t_filler_token(
-					v->token, i - x_origin, j - y_origin)))
-					return (0);
-			}
+			if ((v->token_map[i][j] == '.' || v->token_map[i][j] == '*') &&
+				is_origin_point && !(is_origin_point = 0))
+				set_origin_coordinates(&x_origin, &y_origin, i, j);
+			if (v->token_map[i][j] == '*' && !(v->token = set_t_filler_token(
+				v->token, i - x_origin, j - y_origin)))
+				return (0);
 		}
 	}
 	return (!rewind_t_filler_token(&v->token) ? 0 : 1);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jlereffa <jlereffa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/10 17:36:24 by jlereffa          #+#    #+#             */
-/*   Updated: 2017/08/13 11:22:03 by jlereffa         ###   ########.fr       */
+/*   Updated: 2017/08/13 11:43:16 by jlereffa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,11 @@ int	main(void)
 		if (!get_current_map_and_token_from_standard_entry(&v) ||
 			!extract_token_lst_from_token_map(&v) ||
 			!find_solutions_through_algorithm(&v))
+		{
+			write(1, "0 0\n", 4);
 			return (0);
-		//test_print(&v);
+		}
+		test_print(&v);
 	}
 	return (0);
 }
